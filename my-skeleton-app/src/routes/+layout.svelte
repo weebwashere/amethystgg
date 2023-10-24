35747c19ff819e6c3a09fcb19f04cf1bd9356696
weeb .js <ilyoreo99@gmail.com>
2023-10-24 18:16:17 +0000

    updateds + svelte!

---
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">AMETHYST</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://www.guilded.gg/b/2e702266-2dfe-4796-b61c-ccbb0536444c"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Support Server
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ my-skeleton-app/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Hello!</h1>
+	<p>Introducing.. Amethyst! a fun Interaction guilded bot.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
+</div>
