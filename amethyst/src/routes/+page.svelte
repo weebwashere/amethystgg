fb4c93ff6f61db85e057021a7d16b3045e68a7ae
weebwashere <128880884+weebwashere@users.noreply.github.com>
2024-01-24 19:58:12 -0500

    Update +page.svelte

--- amethyst/src/routes/+page.svelte
+++ amethyst/src/routes/+page.svelte
@@ -32,9 +32,9 @@
 
 <div class="center">
   <div class="container mx-auto p-8 space-y-8">
-    <h1 class="h1 font-abc header">Hello! I'm <span class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">Reminder</span></h1>
-    <p class="font-dm text">A <a class="anchor" href="https://guilded.gg">Guilded</a> reminder bot capable of creating reminders for users 
-    to receive directly from Guilded! :D</p>
+    <h1 class="h1 font-abc header">Hello! I'm <span class="bg-gradient-to-br from-yellow-500 to-yellow-300 bg-clip-text text-transparent box-decoration-clone">Zap</span></h1>
+    <p class="font-dm text">A Multipurpose Guilded bot capable of doing multiple 
+    things! such as fun, utility, and much more.</p>
     <section class="btn-container">
       <a class="btn text-xl font-abc variant-soft-surface rounded py-3 px-7" href="https://www.guilded.gg/b/2e702266-2dfe-4796-b61c-ccbb0536444c">Invite Today!</a>
     </section>
